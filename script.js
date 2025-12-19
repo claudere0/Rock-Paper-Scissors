a06f71c294ace8dffe2d6ee1f7c78af91c4cb215
claudere <129028339+claudere0@users.noreply.github.com>
2025-12-19 23:25:07 +0500

    different random functions from mdn's article

--- script.js
+++ script.js
@@ -9,4 +9,28 @@ function getComputerChoise() {
     }
 }
 
-console.log(getComputerChoise());
+function getHumanChoice() {
+
+}
+
+// console.log(getComputerChoise());
+
+
+
+// function getRandom() {
+//     return Math.random();
+// }
+
+// function getRandomArbitrary(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+// function getRandomInt(min, max) {
+//     const minCeiled = Math.ceil(min);
+//     const maxFloored = Math.floor(max);
+//     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+// }
+
+// console.log(getRandom());
+// console.log(getRandomArbitrary(1,10));
+// console.log(getRandomInt(1,100));
