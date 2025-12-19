24b90bc66cc0dabca04fcc1c6d5093b8aa1fc5c3
claudere <129028339+claudere0@users.noreply.github.com>
2025-12-19 23:19:42 +0500

    function to get computer choise created

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hell yeah I did it!")
+function getComputerChoise() {
+    let choise = Math.floor(Math.random()*3)+1;
+    if (choise === 1) {
+        return "Rock";
+    } else if (choise === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoise());
